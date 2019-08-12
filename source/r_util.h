e82ca27c440c0da3503cf71bf8b994bcaa555e4f
Andrew Bernauer <andrewbernauer94@gmail.com>
2019-08-12 16:21:29 -0500

    C function declarations for CLI utils

--- source/r_util.h
+++ source/r_util.h
@@ -47,9 +47,99 @@
 				 const size_t length);
 
 
+   
+
+   void CLI_SetParamVectorStrStr(const char* paramName,
+				 const char* str,
+				 const size_t element);
+
    void CLI_SetParamVectorInt(const char* paramName,
 			      uint64_t* ints,
 			      const size_t length);
+
+   void CLI_SetParamMat(const char* paramName,
+			double* memptr,
+			const size_t rows,
+			const size_t cols,
+			const bool pointsAsRows);
+
+   void CLI_SetParamUMat(const char* paramName,
+			 size_t* memptr,
+			 const size_t rows,
+			 const size_t cols,
+			 const bool pointsAsRows);
+
+   void CLI_setParamRow(const char* paramName,
+			double* memptr,
+			const size_t cols);
+
+   void CLI_SetParamURow(const char* paramName,
+			 size_t* memptr,
+			 const size_t cols);
+
+   void CLI_SetParamCol(const char* paramName,
+			double* memptr,
+			const size_t rows);
+
+   void CLI_SetParamMatWithInfo(const char* paramName,
+				bool* dimensions,
+				double* memptr,
+				const size_t rows,
+				const size_t cols,
+				const bool pointsAreRows);
+
+   int CLI_GetParamInt(const char* paramName);
+
+   double CLI_GetParamDouble(const char* paramName);
+
+   const char* CLI_GetParamString(const char* paramName);
+
+   bool CLI_GetParamBool(const char* paramName);
+
+   size_t CLI_GetParamVectorStrLen(const char* paramName);
+
+   const char* CLI_GetParamVectorStrStr(const char* paramName, const int i);
+
+   size_t CLI_GetParamVectorIntLen(const char* paramName);
+
+   uint64_t* CLI_GetParamVectorIntPtr(const char* paramName);
+
+   size_t CLI_GetParamMatRows(const char* paramName);
+
+   size_t CLi_GetParamMatCols(const char* paramName);
+
+   double* CLI_GetParamMat(const char* paramName);
+
+   size_t CLI_getParamUMatRows(const char* paramName);
+
+   size_t CLI_getParamUMatCols(const char* paramName);
+
+   size_t* CLI_GetParamUMat(const char* paramName);
+
+   size_t CLI_GetParamColRows(const char* paramName);
+
+   double* CLI_GetParamCol(const char* paramName);
+
+   size_t CLI_GetParamURowCols(const char* paramName);
+
+   size_t* CLI_GetParamURow(const char* paramName);
+
+   size_t CLI_GetParamMatWithInfoRows(const char* paramName);
+
+   size_t CLI_GetParamMatWithInfoCols(const char* paramName);
+
+   bool* CLI_GetParamMatWithInfoBoolPtr(const char* paramName);
+
+   double* CLI_GetParamMatWithInfoPtr(const char* paramName);
+
+   void CLI_EnableVerbose();
+
+   void CLI_DisabelVerbose();
+
+   void CLI_ResetTimers();
+
+   void CLI_SetPassed(const char* paramName);
+   
 
    
 #if defined(__cplusplus) || defined(c_plusplus)
